a5928bff1a9011dfc9feb8c9bcda2e8da652ac39
Ketul Patel <ptl.ketul@gmail.com>
2020-04-19 11:41:33 +0100

    solving https://www.geeksforgeeks.org/count-number-trees-forest/

---
+++ src/com/graph/Forest.java
@@ -0,0 +1,66 @@
+package com.graph;
+
+import java.util.LinkedList;
+
+/**
+ * Program to solve https://www.geeksforgeeks.org/count-number-trees-forest/
+ */
+public class Forest {
+    private int vertex;
+
+    private LinkedList<Integer>[] adjNodes;
+
+    public Forest(int vertex) {
+        this.vertex = vertex;
+        adjNodes = new LinkedList[vertex];
+
+        for(int i=0;i<vertex;i++){
+            adjNodes[i] = new LinkedList<>();
+        }
+    }
+
+    private void addEdge(int from, int to) {
+        adjNodes[from].add(to);
+    }
+
+    private void print() {
+        for(int i =0; i<adjNodes.length; i++){
+            System.out.println(i + " -->" + adjNodes[i].toString());
+        }
+    }
+
+    private void findNoOfTree() {
+        boolean [] visited = new boolean[vertex];
+        int noOfTree = 0;
+        for(int i=0;i<5;i++){
+            if(!visited[i]){
+                boolean isTree = dfs(i, visited, true);
+                System.out.println("is tree for node " + i + " : " + isTree);
+                noOfTree = isTree ? noOfTree+1 : noOfTree;
+            }
+        }
+        System.out.println(noOfTree);
+    }
+
+    private boolean dfs(int i, boolean[] visited, boolean isTree) {
+        visited[i] = true;
+        for(Integer adjNode : adjNodes[i]){
+            if(!visited[adjNode]) {
+                dfs(adjNode, visited, isTree);
+            } else {
+                if(isTree)
+                    isTree = false;
+            }
+        }
+        return isTree;
+    }
+
+    public static void main(String[] args) {
+        Forest forest = new Forest(5);
+        forest.addEdge(0, 1);
+        forest.addEdge(0, 2);
+        forest.addEdge(3, 4);
+        forest.print();
+        forest.findNoOfTree();
+    }
+}
